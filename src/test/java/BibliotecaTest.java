8cbb8340ca520cf2d1e9ce47088f069f97018ade
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-11-08 16:49:20 -0300

    test: Adiciona o teste de existência da classe Biblioteca como parte do desenvolvimento TDD

---
+++ src/test/java/BibliotecaTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BibliotecaTest {
+    @Test
+    void testSearchBooksByTitleWithAvailableCopies() {
+        // Passo 1: Configurar o ambiente de teste
+        Biblioteca biblioteca = new Biblioteca();
+    }
+}
